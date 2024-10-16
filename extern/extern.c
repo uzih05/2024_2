4564248ab2cda713bce81b476c4df4e6e85436fb
Uzih05 <luv.wlgjs@gmail.com>
2024-10-16 09:42:03 +0900

    upload

---
+++ extern/extern.c
@@ -0,0 +1,9 @@
+//
+// Created by Jiheon Yu on 2024. 10. 16..
+//
+
+#include <stdio.h>
+extern ex;
+void sub1(void) {
+    printf("ex의 값은 %d 입니다.\n", ex);
+}
--- 재귀함수(n^k승 계산)/main.c
+++ 재귀함수(n^k승 계산)/main.c
@@ -1,7 +1,15 @@
+#include <math.h>
 #include <stdio.h>
-
-int main(void)
-{
-    printf("Hello, World!\n");
-    return 0;
+int power(int x, int n) {
+    if (n > 0)
+        return x * power(x,n-1);
+    return 1;
+}
+int main(void){
+    int a, b;
+    printf("밑과 지수를 입력하세요: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    power(a, b);
+    printf("%d\n", power(a, b));
 }
